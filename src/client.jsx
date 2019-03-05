6cab7812380cdc0d8360345181b077be7d451703
Henrik Spolander <henrik@spolander.nu>
2019-03-05 21:41:28 +0100

    init commit setup

---
+++ src/client.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Client = () => (
+  <div>
+    <div className="main-content">hej</div>
+  </div>
+);
+
+export default connect(
+  null,
+  null,
+)(Client);
